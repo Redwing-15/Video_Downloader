2977276f27b6d943cf5825321cc6df1624eb2903
Emile Rollé <Rolle.Emile@outlook.com>
2023-12-12 22:43:56 +0000

    small changes

--- testing.py
+++ testing.py
@@ -1,5 +1,10 @@
 from yt_dlp import YoutubeDL
 
 URLS = ['https://www.youtube.com/watch?v=dQw4w9WgXcQ']
-with YoutubeDL() as ydl:
+
+output = {
+  'outtmpl': 'C:/Users/Emile/OneDrive/Proj/Python/Video_Downloader/VidOutput/%(uploader)s/%(title)s.%(ext)s'
+}
+
+with YoutubeDL(output) as ydl:
     ydl.download(URLS)
